5c60e17923d8cb1e983826b7a96599d8d99ce259
Eder Leite <edermfl@gmail.com>
2019-08-16 07:53:00 -0300

    new DislikeController and get devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,25 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports={
+    async index(req, res){
+        console.log(req.headers);
+        if(!req.headers){
+            return res.status(400).json({error: "Header not found!"})
+        }
+        const {user} = req.headers;
+        console.log(user);
+
+        const loggedUser = await Dev.findById(user);
+        const users = await Dev.find({
+            $and:[
+                {_id: {$ne: user} },
+                {_id: {$nin: loggedUser.likes} },
+                {_id: {$nin: loggedUser.dislikes} }
+            ],
+        })
+        return res.json(users);
+    },
+
     async store(req, res){
         const {username} = req.body;
 
--- backend/src/controllers/DislikeController.js
+++ backend/src/controllers/DislikeController.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports={
@@ -6,21 +6,14 @@ module.exports={
         const {devId} = req.params;
         const {user} = req.headers;
 
-        console.log(devId);
-        console.log(user);
-
         const loggedDev = await Dev.findById(user);
-        console.log(loggedDev)
         const targetDev = await Dev.findById(devId);
-        console.log(targetDev)
+
         if(!targetDev){
             return res.status(400).json({error: "Dev not exists"});
         }
-        if(targetDev.likes.includes(loggedDev._id)){
-            console.log("Deu match");
-        }
-
-        loggedDev.likes.push(targetDev._id);
+        
+        loggedDev.dislikes.push(targetDev._id);
 
         await loggedDev.save();
 
