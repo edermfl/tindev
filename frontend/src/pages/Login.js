5553e070494ab9b6ef76f43fdfafb77b253b4252
Eder Leite <edermfl@gmail.com>
2019-08-29 09:06:41 -0300

    tela de login

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css'
+
+import logo from '../asserts/logo.svg';
+
+export default function Login(){
+    return(
+        <div className="login-container" >
+            <form>
+                <img src={logo} alt="Tindev" />
+                <input
+                    placeholder="Digite seu usuário no Github"
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+}
+
